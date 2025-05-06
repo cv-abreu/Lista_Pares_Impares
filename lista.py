7cf0ade1921585fd061013d8202433df5480c7af
[Caroline Vitória Vieira de Abreu] <carolinevv01@gmail.com>
2025-05-06 11:19:09 -0300

    Ajustado

--- lista.py
+++ lista.py
@@ -2,17 +2,20 @@ x=[]
 z=[]
 
 print("-------------------------------------------")
+print(" ")
 print("NÚMEROS PARES E ÍMPARES")
+print(" ")
+print("-------------------------------------------")
 print("> Insira 5 valores a serem classificados")
 print("-------------------------------------------")
 
 for i in range(5):
     i=int(input("> Insira um valor: "))
     if i%2==0:
-        print("*"i," é um número PAR")
+        print("*",i," é um número PAR*")
         x.append(i)
     else:
-        print("+"i, " é um número ÍMPAR*")
+        print("*",i, " é um número ÍMPAR*")
         z.append(i)
 
 print("------------------------------------------")
