068aba785fd153bac06849c4d3cc585085af6729
[Caroline Vitória Vieira de Abreu] <carolinevv01@gmail.com>
2025-05-06 11:10:57 -0300

    Arquivo

---
+++ lista.py
@@ -0,0 +1,21 @@
+x=[]
+z=[]
+
+print("-------------------------------------------")
+print("NÚMEROS PARES E ÍMPARES")
+print("> Insira 5 valores a serem classificados")
+print("-------------------------------------------")
+
+for i in range(5):
+    i=int(input("> Insira um valor: "))
+    if i%2==0:
+        print("*"i," é um número PAR")
+        x.append(i)
+    else:
+        print("+"i, " é um número ÍMPAR*")
+        z.append(i)
+
+print("------------------------------------------")
+print("> Lista de números PARES: ",x)
+print("> Lista de números ÍMPARES: ",z)
+print("------------------------------------------")
